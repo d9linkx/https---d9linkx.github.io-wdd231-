edf487f754887f5b2c8d83fef566a913107a4de2
Prince Dike <parafinix@gmail.com>
2024-10-09 11:10:03 +0100

    new

--- chamber/index.js
+++ chamber/index.js
@@ -1,3 +1,9 @@
-document.querySelector('.ham-menu').addEventListener('click', function() {
-    document.querySelector('.off-screen-menu').classList.toggle('active');
-});
+window.onload = function () {
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile_nav');
+
+    menu_btn.addEventListener('click', function () {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-acive');
+    });
+}
