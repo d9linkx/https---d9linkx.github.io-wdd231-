77d003e5f7ad14d639086cddfd815395430123e8
Prince Dike <parafinix@gmail.com>
2024-10-09 11:27:54 +0100

    .ham error fixed, and performing perfectly

--- chamber/index.js
+++ chamber/index.js
@@ -4,6 +4,6 @@ window.onload = function () {
 
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active');
-        mobile_menu.classList.toggle('is-acive');
+        mobile_menu.classList.toggle('is-active');
     });
 }
